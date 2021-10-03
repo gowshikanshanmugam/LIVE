12031fa3146a2c119c634c21226aa023aa6cb57b
gowshikanshanmugam <91734655+gowshikanshanmugam@users.noreply.github.com>
2021-10-03 10:24:46 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,58 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input7")
+                .setLeft("3.0476190476190474em")
+                .setTop("6.857142857142857em")
+                .setWidth("8.076190476190476em")
+                .setHeight("2.361904761904762em")
+                .setLabelSize("8em")
+                .setLabelCaption("LOGIN")
+                .setMultiLines(true)
+                .setCustomStyle({
+                    "KEY" : {
+                        "color" : "#5F9EA0",
+                        "background-color" : "#F0F8FF"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input8")
+                .setLeft("11.428571428571429em")
+                .setTop("9.904761904761905em")
+                .setWidth("18em")
+                .setHeight("2.361904761904762em")
+                .setLabelSize("8em")
+                .setLabelCaption("Username")
+                .setMultiLines(true)
+                .setCustomStyle({
+                    "KEY" : {
+                        "background-color" : "#B0E0E6"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input9")
+                .setLeft("10.666666666666666em")
+                .setTop("12.19047619047619em")
+                .setWidth("26.36190476190476em")
+                .setLabelSize("8em")
+                .setLabelCaption("Password ")
+                .setType("password")
+                .setCustomStyle({
+                    "KEY" : {
+                        "background-color" : "#B0E0E6"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
